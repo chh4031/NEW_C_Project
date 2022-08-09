e84fb9135f24eb68c74e284444403a7e68aa1db5
chh4031 <chh4031@gmail.com>
2022-08-09 21:09:17 +0900

    2022_08_08

---
+++ 2022_08_08/C_001_class/C_001_class/Hello_world.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+
+	printf("Hello world!\t");
+	printf("Cho_hyeon_ho\n\n");
+
+	printf("3 * 1 = 3\n");
+	printf("3 * 2 = 6\n");
+	printf("3 * 3 = 9\n\n");
+
+	printf("2+5 = %d\n", 2 + 5);
+	printf("2-3 = %d\n", 2 - 3);
+	printf("2*3= %d\n", 2 * 3);
+	printf("2/3 = %d\n", 2 / 3);
+	return 0;
+}
